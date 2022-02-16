1a1192bfc0260774c4553af1a3a43cbb5a8d42dd
carloszaramella <carloszaramella@yahoo.com.br>
2022-02-16 12:35:48 -0300

    first commit

---
+++ src/br/dio/com/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.dio.com;
+
+import br.dio.com.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+
+       /* int a = 5;
+        int b = 3;
+        System.out.println("Hello World! " + (a+b));*/
+    }
+}
